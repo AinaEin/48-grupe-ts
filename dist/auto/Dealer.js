53c4f77231fa1f4eaaae82140edded99396a1ecc
Aina <ainaeinikyte@gmail.com>
2024-05-14 11:14:27 +0300

    feat: change car price

--- dist/auto/Dealer.js
+++ dist/auto/Dealer.js
@@ -8,14 +8,11 @@ class Dealer {
         console.log(`Hi, my name is ${name}!`);
     }
     formatedPrice(price) {
-        // 5 -> 5 Eur
-        // 50 -> 50 Eur
-        // 500 -> 500 Eur
-        // 1500 -> 1.500 Eur
-        // 1050 -> 1.050 Eur
-        // 1005 -> 1.005 Eur
-        let formattedPrice = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-        return price + " " + this.currency;
+        // let formattedPrice = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        return price.toLocaleString('de-DE', {
+            style: "currency",
+            currency: this.currency
+        });
     }
     getCar(model, price) {
         return `New car everyone! ${model} for only ${this.formatedPrice(price)}!`;
